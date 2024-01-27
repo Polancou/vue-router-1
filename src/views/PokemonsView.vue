48df1ec73a824c7e522e144b9b7ba082919432ab
David P. Polanco <davidp@marketshareit.mx>
2024-01-26 18:11:03 -0600

    first backup

---
+++ src/views/PokemonsView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import axios from "axios";
+import {ref} from "vue";
+
+const pokemons = ref([])
+const getData = async () => {
+  try {
+    const data = axios.get('https://pokeapi.co/api/v2/pokemon')
+    console.log(data)
+  } catch (e) {
+    console.error(e)
+  }
+}
+</script>
+
+<template>
+  <h1>Pokemons</h1>
+</template>
+
+<style scoped>
+
+</style>
